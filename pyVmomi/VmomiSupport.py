383dffb23b6a3ccdcff27298f81dca61d33f4aaf
Matthew M. Boedicker <matthewm@boedicker.org>
2015-03-21 13:58:17 -0700

    Inject output stream.

--- pyVmomi/VmomiSupport.py
+++ pyVmomi/VmomiSupport.py
@@ -1,3 +1,5 @@
+import sys
+
 import pyvmomi_to_ruby.or_list
 import pyvmomi_to_ruby.formatter
 
@@ -5,22 +7,22 @@ F_LINK = pyvmomi_to_ruby.or_list.OrList(['link'])
 F_LINKABLE = pyvmomi_to_ruby.or_list.OrList(['linkable'])
 F_OPTIONAL = pyvmomi_to_ruby.or_list.OrList(['optional'])
 
-def CreateDataType(a, b, c, d, e):
+def CreateDataType(a, b, c, d, e, out=sys.stdout):
     if e is not None:
         e = [ list(x) for x in e ]
         for x in e:
             if x[-1] == 0:
                 x.pop()
 
-    print('    create_data_type("{a}", "{b}", "{c}", "{d}", {e})'.format(
+    out.write('    create_data_type("{a}", "{b}", "{c}", "{d}", {e})\n'.format(
         a=a,
         b=b,
         c=c,
         d=d,
         e=pyvmomi_to_ruby.formatter.format_value(e)))
 
-def CreateManagedType(a, b, c, d, e, f):
-    print('    create_managed_type("{a}", "{b}", "{c}", "{d}", {e}, {f})'.format(
+def CreateManagedType(a, b, c, d, e, f, out=sys.stdout):
+    out.write('    create_managed_type("{a}", "{b}", "{c}", "{d}", {e}, {f})\n'.format(
         a=a,
         b=b,
         c=c,
@@ -28,21 +30,21 @@ def CreateManagedType(a, b, c, d, e, f):
         e=pyvmomi_to_ruby.formatter.format_value(e),
         f=pyvmomi_to_ruby.formatter.format_value(f)))
 
-def CreateEnumType(a, b, c, d):
-    print('    create_enum_type("{a}", "{b}", "{c}", {d})'.format(
+def CreateEnumType(a, b, c, d, out=sys.stdout):
+    out.write('    create_enum_type("{a}", "{b}", "{c}", {d})\n'.format(
         a=a,
         b=b,
         c=c,
         d=pyvmomi_to_ruby.formatter.format_value(d)))
 
-def AddVersion(a, b, c, d, e):
+def AddVersion(a, b, c, d, e, out=sys.stdout):
     if d == 0:
         d = 'false'
     else:
         d = 'true'
 
-    print('    add_version("{}", "{}", "{}", {}, "{}")'.format(
+    out.write('    add_version("{}", "{}", "{}", {}, "{}")\n'.format(
         a, b, c, d, e))
 
-def AddVersionParent(a, b):
-    print('    add_version_parent("{}", "{}")'.format(a, b))
+def AddVersionParent(a, b, out=sys.stdout):
+    out.write('    add_version_parent("{}", "{}")\n'.format(a, b))
