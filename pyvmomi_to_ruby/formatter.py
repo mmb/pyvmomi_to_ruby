80aa30176bc5ffd61ae6ad3fd8579e923d2292fb
Matthew M. Boedicker <matthewm@boedicker.org>
2015-03-23 22:59:49 -0700

    Switch default case in format_value().

--- pyvmomi_to_ruby/formatter.py
+++ pyvmomi_to_ruby/formatter.py
@@ -1,16 +1,12 @@
-from . import or_list
-
 def format_value(v):
     if v is None:
         return 'nil'
-    elif type(v) is or_list.OrList:
-        return str(v)
     elif type(v) is list or type(v) is tuple:
         return format_list(v)
-    elif type(v) is int:
-        return '{0}'.format(v)
-    else:
+    elif type(v) is str:
         return '"{0}"'.format(v)
+    else:
+        return '{0}'.format(v)
 
 def format_list(alist):
     inner = ', '.join([ format_value(v) for v in alist ])
